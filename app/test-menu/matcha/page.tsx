bb6cfa5987365b11fb8469f8274714af07a04757
asil <asilchik18@gmail.com>
2026-07-16 13:31:24 +0500

    design: удаление лишних тем и полировка классической темы

--- app/test-menu/matcha/page.tsx
+++
@@ -1,7 +0,0 @@
-import MenuApp from "@/components/MenuApp";
-import menu from "@/data/menu.json";
-import type { MenuData } from "@/lib/types";
-
-export default function MatchaMenuPage() {
-  return <MenuApp menu={menu as MenuData} theme="matcha" />;
-}
